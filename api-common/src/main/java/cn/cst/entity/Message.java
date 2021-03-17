3bde330280c56dfd815285772021624ff4549cf3
Shaotong chen <13144061737@163.com>
2021-03-17 22:36:05 +0800

    '首次上传'

---
+++ api-common/src/main/java/cn/cst/entity/Message.java
@@ -0,0 +1,16 @@
+package cn.cst.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String msg = "this is msg";
+
+}
